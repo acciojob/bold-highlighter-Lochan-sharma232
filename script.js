8382285ccef1b668b60160778f7bc992d0d48f52
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-04 22:49:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function highlight() {
     //Write your code here
-var job=document.getElementByTagName("strong");
-	var index=0;length=job.length;
+	let job=document.getElementByTagName("strong");
+	
 	for (let index = 0; index < length; index++){
 		job[index].style.color="green";
-	}()
+	}
 		function return_normal() {
     //Write your code here
 
